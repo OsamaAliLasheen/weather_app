bb746006d4a5e69d543085ed6ee7430fe024f664
Osama <osama.ali.lasheen@gmail.com>
2024-06-02 14:24:38 +0300

    Add weather icons and update UI layout in WeatherDisplayPage

--- lib/views/weather_display_page.dart
+++ lib/views/weather_display_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WeatherDisplay extends StatelessWidget {
   const WeatherDisplay({super.key});
@@ -6,16 +7,65 @@ class WeatherDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: [
-            Colors.blue,
-            Colors.lightBlueAccent,
-            Color.fromARGB(255, 160, 233, 253),
-          ],
+        home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blue,
+              Colors.lightBlueAccent,
+              Color.fromARGB(255, 160, 233, 253),
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Alexandria',
+                    style:
+                        TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+                const Text('updated at 12:00',
+                    style: TextStyle(
+                      fontSize: 22,
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset('assets/images/clear.png'),
+                    const Text(
+                      '18',
+                      style:
+                          TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                    ),
+                    const Column(
+                      children: [
+                        Text(
+                          'Maxtemp: 25',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          'Mintemp: 15',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                const Text(
+                  'Sunny',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     ));
