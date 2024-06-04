c25be6c8a475f88e68b3a2cac04f56f1322dc0b3
Osama <osama.ali.lasheen@gmail.com>
2024-06-04 14:48:40 +0300

    Refactor getCurrentWeather method to handle exceptions and return null if there is an error.

--- lib/Services/weather_service.dart
+++ lib/Services/weather_service.dart
@@ -7,10 +7,16 @@ class WeatherService {
   final String apiKey = 'eccd7fd7f71449f69f2164415240306';
   WeatherService(this.dio);
 
-  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
-    Response response =
-        await dio.get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+      if (response.statusCode == 200) {
+        WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+        return weatherModel;
+      } else {}
+    } on Exception catch (e) {
+      return null;
+    }
   }
 }
