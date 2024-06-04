f51c03e3a091a724623fd9986b2f47b28d4231b9
Osama <osama.ali.lasheen@gmail.com>
2024-06-04 14:27:56 +0300

    Add WeatherService class and dio dependency to fetch current weather data by city name.

--- lib/Services/weather_service.dart
+++ lib/Services/weather_service.dart
@@ -1,1 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/Models/weather_model.dart';
 
+class WeatherService {
+  final Dio dio;
+
+  WeatherService(this.dio);
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    Response response = await dio.get(
+        'https://api.weatherapi.com/v1/forecast.json?key=eccd7fd7f71449f69f2164415240306&q=$cityName&days=1');
+    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    return weatherModel;
+  }
+}
